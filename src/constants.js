7a0a88916dca77ce8ffd8cdf381896f354699cb1
Artizno <artiz@ecio.space>
2021-07-24 21:54:55 +0700

    Add Ecio NFT Card

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const HOST = 'https://birthstamps.herokuapp.com'
+const HOST = 'https://ec2-13-250-56-15.ap-southeast-1.compute.amazonaws.com:5000'
 
 module.exports = {
   HOST
--- src/database.js
+++ src/database.js
@@ -1,22 +1,6 @@
 module.exports = {
-  "1": {
-    "name": "Chunky Monkey",
-    "birthday": "10-18"
-  },
-  "2": {
-    "name": "Flerp Derp",
-    "birthday": "01-08"
-  },
-  "3": {
-    "name": "Buttle McFuddle",
-    "birthday": "01-22"
-  },
-  "4": {
-    "name": "Quiero Pancakes",
-    "birthday": "03-04"
-  },
-  "5": {
-    "name": "Guino the Penguin",
-    "birthday": "03-09"
+  "0": {
+    "name": "ECIO Common Box NFT",
+    "description":"Participate in the Airdrop and get a chance to win NFT and ECIO Tokens. Limited Amount available Only for ECIO Space Crew."
   }
 }

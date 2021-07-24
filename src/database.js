0ac58f6e0cebb97176585ecaabc6360e5f0d738b
Artizno <artiz@ecio.space>
2021-07-24 23:08:04 +0700

    Add new NFT

--- src/database.js
+++ src/database.js
@@ -2,5 +2,15 @@ module.exports = {
   "0": {
     "name": "ECIO Common Box NFT",
     "description":"Participate in the Airdrop and get a chance to win NFT and ECIO Tokens. Limited Amount available Only for ECIO Space Crew."
+  },
+  "1": {
+    "name": "ECIO Background #1 NFT",
+    "description":"Limited Edition Backgroud NFT"
+  },  
+  "2": {
+    "name": "ECIO Common Box #2 NFT",
+    "description":"Limited Edition Backgroud NFT"
   }
 }
+
+ 
